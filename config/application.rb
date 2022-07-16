e58659c091a5756134126a562201200f8f627135
Hピエトロ <awpak1208h@gmail.com>
2022-07-17 03:04:28 +0900

    add タイムゾーンを東京に変更

--- config/application.rb
+++ config/application.rb
@@ -36,6 +36,9 @@ module InstaCloneVer7
     # Don't generate system test files.
     config.generators.system_tests = nil
 
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
+
     config.i18n.default_locale = :ja
   end
 end
