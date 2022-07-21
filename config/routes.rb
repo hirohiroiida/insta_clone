789548170266dd6b67947a77ad046c2023aeddd7
Hピエトロ <awpak1208h@gmail.com>
2022-07-22 04:07:44 +0900

    add 投稿記事(post)についてのコントローラとモデルを作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'posts/index'
+  get 'posts/new'
+  get 'posts/show'
+  get 'posts/edit'
   get '/health_check', to: 'health_checks#show'
   root to: 'samples#index'
   get '/signup', to: 'users#new'
