feb64ec46a09c757d29a5981e198d5276b4fe37a
Hピエトロ <awpak1208h@gmail.com>
2022-08-12 13:03:10 +0900

    add フォロー, アンフォローの機能を実装した

---
+++ app/controllers/user/relationships_controller.rb
@@ -0,0 +1,13 @@
+class User::RelationshipsController < ApplicationController
+  before_action :require_login
+
+  def create
+    @user = User.find(params[:user_id])
+    current_user.follow(@user)
+  end
+
+  def destroy
+    @user = User.find(params[:user_id])
+    current_user.unfollow(@user)
+  end
+end
