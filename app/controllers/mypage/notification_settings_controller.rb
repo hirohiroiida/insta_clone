713899e58c5ec8427c7e3b78d4ffb1bd11022ae1
Hピエトロ <awpak1208h@gmail.com>
2022-10-14 12:43:20 +0900

    modify NotificationSettingsController < Mypage::BaseController にした

--- app/controllers/mypage/notification_settings_controller.rb
+++ app/controllers/mypage/notification_settings_controller.rb
@@ -1,4 +1,4 @@
-class Mypage::NotificationSettingsController < ApplicationController
+class Mypage::NotificationSettingsController < Mypage::BaseController
   def edit
     @user = User.find(current_user.id)
   end
