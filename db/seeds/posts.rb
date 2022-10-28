39665ad68f6ed6205770ff391720ec0d4dc98f19
Hピエトロ <awpak1208h@gmail.com>
2022-10-28 22:20:48 +0900

    modify seedを変更

--- db/seeds/posts.rb
+++ db/seeds/posts.rb
@@ -1,6 +1,6 @@
 puts 'Start inserting seed "posts" ...'
-User.limit(5).each do |user|
-  9.times do |n|
+User.limit(500).each do |user|
+  2.times do |n|
     post = user.posts.build(body: Faker::Hacker.say_something_smart)
     post.images.attach(io: File.open('db/fixtures/dummy_1.jpg'), filename: 'dummy')
     post.images.attach(io: File.open('db/fixtures/dummy_2.jpg'), filename: 'dummy')
